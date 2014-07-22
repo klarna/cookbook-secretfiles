4f07eaece935db48f1e7a5e554d3d1baa2749732
Carl Loa Odin <carlodin@gmail.com>
2014-07-22 15:19:57 +0200

    Bump version to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'carlodin@gmail.com'
 license          'Apache License, Version 2.0'
 description      'Utility cookbook to decrypt secrets onto the filesystem'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
